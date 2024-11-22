20442ea7ce7511e82c2824346be55631cd752e1c
Quentin Verdier <quent1.verdier@gmail.com>
2024-11-22 15:25:00 +0100

    refactor: remove unused Tailwind CSS configuration and global styles; add new UI components and utilities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Input } from '@/components/ui/input';
+
 export default function Home() {
   return (
-    <main>
-      <h1>Instant mate admin</h1>
+    <main className="container mx-auto flex flex-col gap-8 pt-6">
+      <h1 className="text-primary">Instant mate admin</h1>
       <div className="flex gap-4">
-        <input type="text" placeholder="Nom" className="input input-bordered w-full max-w-xs" />
+        <Input type="email" placeholder="Email" />
       </div>
     </main>
   );
